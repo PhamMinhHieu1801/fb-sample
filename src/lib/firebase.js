2b427f900ff5967cf09c5c632f6964810316a7d9
EC2 Default User <ec2-user@ip-172-31-71-94.ec2.internal>
2021-05-21 10:14:07 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+ import firebase from "firebase";
+ var firebaseConfig = {
+    apiKey: "AIzaSyCtgVKIGkt8a4az4cJpQqKzc4lOg9YFyU8",
+    authDomain: "fb-sample-44100.firebaseapp.com",
+    projectId: "fb-sample-44100",
+    storageBucket: "fb-sample-44100.appspot.com",
+    messagingSenderId: "1096820391875",
+    appId: "1:1096820391875:web:5ff2dbc53ecd9dbefc8776"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
